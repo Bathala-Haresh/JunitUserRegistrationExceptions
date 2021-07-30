8466efada194093f5afb53c70471181447467966
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-31 01:17:51 +0530

    Added:Junit test cases with exceptions for UserRegistration

---
+++ src/main/java/userregistration/UserRegitrationException.java
@@ -0,0 +1,15 @@
+package userregistration;
+
+public class UserRegitrationException extends RuntimeException {
+    public enum ExceptionType
+    {
+        ENTERED_NULL,ENTERED_EMPTY
+    }
+
+    public ExceptionType type;
+
+    public UserRegitrationException(ExceptionType type,String message) {
+        super(message);
+        this.type = type;
+    }
+}
